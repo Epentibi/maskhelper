f30d2a11d12508e3ef92cde21208c2d1dfd1aa19
Jingcheng <43139384+Epentibi@users.noreply.github.com>
2020-02-07 23:02:01 +0800

    New images and intergration
    添加照片

--- pages/home/home.js
+++ pages/home/home.js
@@ -8,42 +8,8 @@ Page({
 
     imgUrls: [
       '../../images/Page/Battle.jpg',
-
       '../../images/Page/Battle3.jpg',
       '../../images/Page/Battle5.jpg'
     ],
   },
-  
-
-
-
-
-
-
-data:{
-array:[
-  {
-    "img":'../../images/Search List/N95.jpg',
-    "title":'N95 口罩',
-    "function":'过滤效率大于95%',
-    "price":'大约12元',
-    "time" :'4小时一换'
-  
-  },
-  {
-    "img": '../../images/Search List/waike.jpg',
-    "title": '医用外科口罩',
-    "function":'过滤效率大于70%',
-    "price": '3-4元',
-    "time": '4小时一换'
-  },
-  {
-    "img": '../../images/Search List/mian.jpg',
-    "title": '其他种类口罩',
-    "function": '过滤效率大于30%',
-    "price": '价格不等',
-    "time": '使用时间不等'
-  }
-]
-}
 })
